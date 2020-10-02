b52ab20db774261e5161818898892d7823f352ed
nightlaro <mickocabacungan@gmail.com>
2020-10-02 16:55:39 -0700

    created database server

---
+++ server/databaseServer.js
@@ -0,0 +1,53 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const {getReviewsByGameIdWithUsersAndBadges } = require('../db/index');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/gamereviews/:gameid', async (req, res) => {
+  let { gameid } = req.params;
+  console.log(req.originalUrl)
+  if (parseInt(gameid) <= 0 || parseInt(gameid) > 30000) {
+    res.status(400).json({ error: 'Invalid game ID. Please use a number between 1 and 30000.' });
+    return;
+  }
+  try {
+    let payload = await getReviewsByGameIdWithUsersAndBadges(gameid, req.query);
+
+    console.log(payload.data.length);
+
+    let helpful;
+    let recent;
+    if ((req.query.display && req.query.display === 'summary') || !req.query.display) {
+      recent = payload.data.slice().sort((a, b) => Date.parse(a.date_posted) > Date.parse(b.date_posted) ? -1 : 1);
+      payload.recent = recent;
+    }
+
+    res.status(200).json(payload);
+
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Error retrieving reviews' });
+  }
+});
+
+app.post('/create/:id_game', (req, res) => {
+  let options = req.body;
+  console.log("Inserting new reviews");
+  //console.log(options);
+  createNewReview(options).then((result) => {
+    console.log("Success!");
+    res.send(201);
+  }).catch(e => {
+    console.error(e)
+    res.send(501);
+  });
+
+})
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 4000}`);
+});
--- server/index.js
+++ server/index.js
@@ -14,28 +14,28 @@ const { getPurchaseTypeDataForGameId, getReviewsByGameIdWithOptions, getUserById
 //const { asyncForEach } = require('./asyncForEach');
 
 //implementing clustering
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-console.log(`We have ${numCPUs} available`);
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus().length;
+// console.log(`We have ${numCPUs} available`);
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running`);
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  console.log(`Worker ${process.pid} started`);
-  app.listen(3001, () => {
-    console.log('Your node is running on port 3001');
-  });
-}
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//   });
+// } else {
+//   console.log(`Worker ${process.pid} started`);
+//   app.listen(3001, () => {
+//     console.log('Your node is running on port 3001');
+//   });
+// }
 
 app.use(bodyParser.json());
-app.use('/api', router);
+//app.use('/api', router);
 app.use(express.json());
 app.use(cors());
 app.use('/', expressStaticGzip(path.resolve(__dirname, '..', 'public')));
@@ -45,85 +45,76 @@ app.get('/app/:gameid', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
-router.get('/gamereviews/:gameid', async (req, res) => {
+app.get('/gamereviews/:gameid', (req, res) => {
   let { gameid } = req.params;
-  console.log(req.originalUrl)
-  if (parseInt(gameid) <= 0 || parseInt(gameid) > 30000) {
-    res.status(400).json({ error: 'Invalid game ID. Please use a number between 1 and 30000.' });
-    return;
-  }
-  try {
-    // Get steamPurchased, otherPurchased counts via db call
-    // let { direct, key } = await getPurchaseTypeDataForGameId(gameid);
-
-    // // Get review data, sorted/filtered according to URLSearchParams
-    // let data = await getReviewsByGameIdWithOptions(gameid, req.query);
-
-    let payload = await getReviewsByGameIdWithUsersAndBadges(gameid, req.query);
-    console.log(payload.data.length);
-
-    // Attach user, badge info to each review with separate db calls
-    // asyncForEach > forEach b/c forEach doesn't work well with asynchronous callbacks
-
-    // await asyncForEach(data, async (review) => {
-    //   let [ user ] = await getUserById(review.id_user);
-    //   review.user = user;
-    //   if (review.user.id_badge) {
-    //     let [ badge ] = await getBadgeById(review.user.id_badge);
-    //     review.user.badge = badge;
-    //   }
-    // });
-
-    // If req.query doesn't contain display, or display === 'summary',
-    // data needs to be copied & sorted by recently posted after asyncForEach
-    let helpful;
-    let recent;
-    if ((req.query.display && req.query.display === 'summary') || !req.query.display) {
-      // helpful = data;
-      // recent = data.slice().sort((a, b) => Date.parse(a.date_posted) > Date.parse(b.date_posted) ? -1 : 1);
-      recent = payload.data.slice().sort((a, b) => Date.parse(a.date_posted) > Date.parse(b.date_posted) ? -1 : 1);
-      payload.recent = recent;
-    }
-    //console.log(payload);
+  fetch(`http://3.137.180.221:3001/gamereviews/${gameid}`).then(payload => {
+    console.log('Got data!')
     res.status(200).json(payload);
-
-    // res.status(200).json({
-    //   steamPurchasedCount: direct,
-    //   otherPurchasedCount: key,
-    //   data: helpful || data,
-    //   recent
-    // });
-  } catch (e) {
-    console.error(e);
+  })
+  .catch(err => {
+    console.error(err);
     res.status(500).json({ error: 'Error retrieving reviews' });
-  }
-});
+  })
+})
 
-// router.get('/reviewcount/:gameid', (req, res) => {
-//   fetch(`http://ec2-54-185-79-51.us-west-2.compute.amazonaws.com:3002/api/reviewcount/${req.params.gameid}`)
-//     .then(response => response.json())
-//     .then(results => {
-//       res.status(200).json(results);
-//     })
-//     .catch(e => {
-//       console.error(e);
-//       res.status(500).json({ error: 'Error fetching review counts' });
-//     });
-// });
-
-router.post('/create/:id_game', (req, res) => {
+app.post('/create/:id_game', (req, res) => {
   let options = {
     ...req.params,
     ...req.body
   }
-  console.log("Inserting new reviews");
-  //console.log(options);
-  createNewReview(options).then((result) => {
-    console.log("Success!");
-    res.send(201);
-  }).catch(e => console.error(e));
-
+  fetch(`http://3.137.180.221:3001/create/${gameid}`, {
+    method: 'post',
+    body: JSON.stringify(options),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  .then(res => {
+    console.log("Success posting");
+    res.status(201);
+  })
+  .catch(e => {
+    console.error(e);
+    res.status(500);
+  })
 })
+// router.get('/gamereviews/:gameid', async (req, res) => {
+//   let { gameid } = req.params;
+//   console.log(req.originalUrl)
+//   if (parseInt(gameid) <= 0 || parseInt(gameid) > 30000) {
+//     res.status(400).json({ error: 'Invalid game ID. Please use a number between 1 and 30000.' });
+//     return;
+//   }
+//   try {
+
+//     let payload = await getReviewsByGameIdWithUsersAndBadges(gameid, req.query);
+//     console.log(payload.data.length);
+
+//     let helpful;
+//     let recent;
+//     if ((req.query.display && req.query.display === 'summary') || !req.query.display) {
+//       recent = payload.data.slice().sort((a, b) => Date.parse(a.date_posted) > Date.parse(b.date_posted) ? -1 : 1);
+//       payload.recent = recent;
+//     }
+//     res.status(200).json(payload);
+
+//   } catch (e) {
+//     console.error(e);
+//     res.status(500).json({ error: 'Error retrieving reviews' });
+//   }
+// });
+
+// router.post('/create/:id_game', (req, res) => {
+//   let options = {
+//     ...req.params,
+//     ...req.body
+//   }
+//   console.log("Inserting new reviews");
+//   //console.log(options);
+//   createNewReview(options).then((result) => {
+//     console.log("Success!");
+//     res.send(201);
+//   }).catch(e => console.error(e));
+
+// })
 
 router.patch('/update/:id', (req, res) => {
   let options = {
