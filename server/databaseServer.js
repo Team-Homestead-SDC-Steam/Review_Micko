b6044d27073354ac3b310ed3f194de239068d136
nightlaro <mickocabacungan@gmail.com>
2020-10-02 17:34:34 -0700

    added method to db server

--- server/databaseServer.js
+++ server/databaseServer.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const {getReviewsByGameIdWithUsersAndBadges } = require('../db/index');
+const { getReviewsByGameIdWithUsersAndBadges, createNewReview } = require('../db/index');
 
 app.use(bodyParser.json());
 app.use(cors());
